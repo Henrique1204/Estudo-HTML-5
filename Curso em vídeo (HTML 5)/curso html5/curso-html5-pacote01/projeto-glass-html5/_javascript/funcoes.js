cedd8831e06d2591e61f4dd755f31a5f3abe8dea
Paulo <pauloh16.pdr@gmail.com>
2019-11-13 21:25:05 -0300

    Curso em video(HTML 5): finalizar curso

--- Curso em vídeo (HTML 5)/curso html5/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
+++ Curso em vídeo (HTML 5)/curso html5/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,10 @@
 function mudaFoto(foto){
     document.getElementById('icone').src = foto;    
 };
+
+function calcTotal(){
+    let qtd = Number(document.getElementById('cQnt').value);
+    let total = qtd * 1500;
+    let inputTot = document.getElementById('cTot');
+    inputTot.value = total;
+}
